202d018ac2c3e45ae004ca12159f1e4b84e200a3
mohammednavid <navidmohd5@gmail.com>
2025-11-01 16:20:48 +0530

    deposit auto & withdraw manual flow updated

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,7 +1,5 @@
 export const explorerUrl = "https://hyperevmscan.io";
 
-export const operatorAddress = "0x2CdC7b3039bA87F1eb1BCeA36907f9651821Cba4";
-
 import yieldMonitorService from "@/services/vaultService";
 import { LatestVaultsResponse, LatestVaultItem } from "@/services/config";
 
